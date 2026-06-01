5cffcf995b75351580c3b4542a930a8572f3fee6
samipabMagar <samipab98@gmail.com>
2026-06-01 23:55:29 +0545

    Add redirect logic for unauthenticated users accessing the dashboard

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -54,5 +54,11 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
+  if(request.nextUrl.pathname === "/dashboard" && !user) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/signin";
+    return NextResponse.redirect(url);
+  }
+
   return supabaseResponse;
 }
